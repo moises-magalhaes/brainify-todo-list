bc268462f7ca5cdde09cde7e0a26925d0cb8cad1
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2021-11-11 23:37:04 +0100

    adding and removing

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import { RiCloseCircleLine } from "react-icons/ri";
+
+const Todo = ({ todos, completeTodo, removeTodo }) => {
+	return todos.map((todo, index) => (
+		<div
+			className={todo.isComplete ? "todo-row complete" : "todo-row"}
+			key={index}
+		>
+			<div key={todo.id} onClick={() => completeTodo(todo.id)}>
+				{todo.text}
+			</div>
+			<div className="icons">
+				<RiCloseCircleLine
+					onClick={() => removeTodo(todo.id)}
+					className="delete-icon"
+				/>
+			</div>
+		</div>
+	));
+};
+
+export default Todo;
---
+++ src/components/TodoList.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+
+const TodoList = () => {
+	const [todos, setTodos] = useState([]);
+
+	const addTodo = (todo) => {
+		if (!todo.text || /^\s$/.test(todo.text)) {
+			return;
+		}
+		const newTodos = [todo, ...todos];
+		setTodos(newTodos);
+		// console.log(...todos);
+	};
+
+	const removeTodo = (id) => {
+		const removeArr = [...todos].filter((todo) => todo.id !== id);
+		setTodos(removeArr);
+	};
+
+	const completeTodo = (id) => {
+		let updatedTodos = todos.map((todo) => {
+			if (todo.id === id) {
+				todo.isComplete = !todo.isComplete;
+			}
+			return todo;
+		});
+		setTodos(updatedTodos);
+	};
+
+	return (
+		<div>
+			<h1>My to-do-list</h1>
+			<TodoForm onSubmit={addTodo} />
+			<Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+		</div>
+	);
+};
+
+export default TodoList;
